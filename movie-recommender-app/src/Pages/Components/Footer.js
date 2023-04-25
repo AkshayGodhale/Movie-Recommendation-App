7cc72e7a6082dab3caf14d64f78e2fee0e9f91a8
Akshay Godhale <akshaygodhale@gmail.com>
2023-04-25 15:05:26 +0530

    1 comit

---
+++ movie-recommender-app/src/Pages/Components/Footer.js
@@ -0,0 +1,39 @@
+// Footer Component
+import "./styles/Footer.css";
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footer">
+                <div id="row1" className="row ">
+                    <a
+                        href="https://www.linkedin.com/in/manish-thakare-90681a22b/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <i className="fa fa-linkedin"></i>
+                    </a>
+                    <a
+                        href="https://github.com/AkshayGodhale/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <i className="fa-brands fa-github"></i>
+                    </a>
+                    <a
+                        href="https://instagram.com/manish_thakare_12/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <i className="fa fa-instagram"></i>
+                    </a>
+                </div>
+                <div id="row1" className="row">
+                    © Developed By Manish Thakare
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
